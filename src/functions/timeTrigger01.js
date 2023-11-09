57fd0416c743bdbb13954036b31395471ccdfc1c
watcharin kobkoy <zwatcharin.k@pttdigital.com>
2023-11-09 15:39:33 +0700

    add timeTrigger

---
+++ src/functions/timeTrigger01.js
@@ -0,0 +1,8 @@
+const { app } = require('@azure/functions');
+
+app.timer('timeTrigger01', {
+    schedule: '*/30 * * * * *',
+    handler: (myTimer, context) => {
+        context.log('Timer function processed request.');
+    }
+});
