4b284ba4852ce125b8d7ec9c9060d75c56da6b5a
watcharin kobkoy <zwatcharin.k@pttdigital.com>
2023-11-09 16:23:09 +0700

    edit cronjob

--- src/functions/timeTrigger01.js
+++ src/functions/timeTrigger01.js
@@ -1,7 +1,7 @@
 const { app } = require('@azure/functions');
 
 app.timer('timeTrigger01', {
-    schedule: '01 * * * * *',
+    schedule: '*/30 * * * *',
     handler: (myTimer, context) => {
         context.log('Timer function processed request.');
     }
